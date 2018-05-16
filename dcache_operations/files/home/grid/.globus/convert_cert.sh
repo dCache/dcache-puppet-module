a1546d74c43fbd3cb802988e5040e80de45533c4
Christian Voss <christian.voss@desy.de>
2018-05-16 16:16:24 +0200

    Added DESY dcache operations puppet module.

--- dcache_operations/files/home/grid/.globus/convert_cert.sh
+++
@@ -1,6 +0,0 @@
-## cert conversion see: htcondor/documentation/grid_certificate_conversion_p12-2-pem.pdf
-# openssl pkcs12 -in usercert.p12 -nokeys -clcerts -out ~/.globus/usercert.pem
-# openssl pkcs12 -in usercert.p12 -nocerts  -out ~/.globus/userkey.pem
-## remove passwort from keyfile:
-# openssl pkcs12 -export -in pem_with_pass.pem -out pem_without_pass.p12
-# openssl rsa -in userkey.pem -out /etc/ssl/private/userkey.nocrypt.key
